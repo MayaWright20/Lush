9e976b5bbcfbd82d09560a7923a7beaf4edf822b
mayagwright20 <mayagwright20@hotmail.com>
2025-11-15 03:03:31 +0000

    Add Lush font creater

---
+++ components/lush-font.tsx
@@ -0,0 +1,24 @@
+import {
+    PermanentMarker_400Regular,
+    useFonts,
+} from "@expo-google-fonts/permanent-marker";
+import React from "react";
+import { StyleSheet, Text, TextProps } from "react-native";
+
+export function LushFont(props: TextProps) {
+  const [fontLoaded] = useFonts({
+    LushFont: PermanentMarker_400Regular,
+  });
+
+  if (!fontLoaded) {
+    return <Text {...props} style={styles.defaultFont} />;
+  }
+  return <Text {...props} style={[{ fontFamily: "LushFont" }, props.style]} />;
+}
+
+const styles = StyleSheet.create({
+  defaultFont: {
+    fontWeight: "bold",
+    textDecorationLine: "underline",
+  },
+});
