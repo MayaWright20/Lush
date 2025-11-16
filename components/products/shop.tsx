92ff3a7dfd95b51957f48a9ca14d939b899b6c9d
mayagwright20 <mayagwright20@hotmail.com>
2025-11-16 22:21:49 +0000

    Remove unnecessary code from shop

--- components/products/shop.tsx
+++ components/products/shop.tsx
@@ -13,17 +13,15 @@ interface Props {
 
 export default function Shop({ products }: Props) {
   return (
-    <>
-      <CheckedBackground isOnlyBorders borderColor={COLORS.BLUE}>
-        <FlatList
-          numColumns={2}
-          data={products || []}
-          renderItem={({ item }) => <ProductItem item={item} />}
-          keyExtractor={(item) => item.id?.toString() || ""}
-          columnWrapperStyle={styles.container}
-        />
-      </CheckedBackground>
-    </>
+    <CheckedBackground isOnlyBorders borderColor={COLORS.BLUE}>
+      <FlatList
+        numColumns={2}
+        data={products || []}
+        renderItem={({ item }) => <ProductItem item={item} />}
+        keyExtractor={(item) => item.id?.toString() || ""}
+        columnWrapperStyle={styles.container}
+      />
+    </CheckedBackground>
   );
 }
 
