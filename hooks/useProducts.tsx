152a1010f8e01611a6c0352eeca6ff1634576e7e
mayagwright20 <mayagwright20@hotmail.com>
2025-11-16 21:35:40 +0000

    Move Product interfaces to type.ts file

--- hooks/useProducts.tsx
+++ hooks/useProducts.tsx
@@ -1,41 +1,5 @@
 import { StoreState, useStore } from "@/store/store";
 
-interface Thumbnail {
-  url: string;
-  alt: string;
-  __typename?: string;
-}
-
-interface ProductType {
-  slug: string;
-  hasVariants: boolean;
-  __typename?: string;
-}
-
-interface Collection {
-  name: "Conditioners";
-  slug: "conditioners";
-  __typename: "SaleorSimpleCollection";
-}
-
-export interface Product {
-  category?: string[];
-  description?: string;
-  minPrice?: string;
-  maxPrice?: string;
-  currency?: string;
-  slug: string;
-  id: string;
-  objectID: string;
-  name: string;
-  isAvailableForPurchase: boolean;
-  isAvailable: boolean;
-  thumbnail: Thumbnail;
-  productType: ProductType;
-  collections: Collection[];
-  __typename: string;
-}
-
 export default function useProducts() {
   const products = useStore((state: StoreState) => state.products);
   const setProducts = useStore((state: StoreState) => state.setProducts);
---
+++ types.ts
@@ -0,0 +1,35 @@
+interface Thumbnail {
+  url: string;
+  alt: string;
+  __typename?: string;
+}
+
+interface ProductType {
+  slug: string;
+  hasVariants: boolean;
+  __typename?: string;
+}
+
+interface Collection {
+  name: "Conditioners";
+  slug: "conditioners";
+  __typename: "SaleorSimpleCollection";
+}
+
+export interface Product {
+  category?: string[];
+  description?: string;
+  minPrice?: string;
+  maxPrice?: string;
+  currency?: string;
+  slug: string;
+  id: string;
+  objectID: string;
+  name: string;
+  isAvailableForPurchase: boolean;
+  isAvailable: boolean;
+  thumbnail: Thumbnail;
+  productType: ProductType;
+  collections: Collection[];
+  __typename: string;
+}
