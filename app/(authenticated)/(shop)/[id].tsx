44de28ba5542892130b10ab932aa08718bdeeb94
mayagwright20 <mayagwright20@hotmail.com>
2025-11-17 03:38:59 +0000

    Add description to product

--- app/(authenticated)/(shop)/[id].tsx
+++ app/(authenticated)/(shop)/[id].tsx
@@ -1,5 +1,6 @@
 import { useLocalSearchParams } from "expo-router";
 import { Image, ScrollView, StyleSheet } from "react-native";
+import RenderHtml from "react-native-render-html";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 import CheckedBackground from "@/components/backgrounds/checked-background";
@@ -8,7 +9,6 @@ import { LushFont } from "@/components/lush-font";
 import SoldOutLabel from "@/components/shop/sold-out-label";
 import { COLORS } from "@/constants/colors";
 import { PADDING_HORIZONTAL_PAGE } from "@/constants/styles";
-import { RenderedHtml } from "@/utils/render-html";
 
 export default function Product() {
   const { product } = useLocalSearchParams<{
@@ -18,6 +18,9 @@ export default function Product() {
   const item = product ? JSON.parse(product) : null;
 
   console.log("lksadjfl", typeof item.description);
+
+  const html = item.description;
+  // const html = `<b>How to use:<br></b>Up the ante with our first ever vitamin C product. This dual-use foaming facial scrub is made with vitamin C powder for its anti-oxidant and anti-inflammatory qualities. This exfoliating face scrub has a 10% vitamin C formula, which may tingle during use as it exfoliates dead skin cells. For maximum effect, use as a mask and apply to a clean, dry face, leaving for 1-2 minutes. Rinse off with water and pat dry. For a more gentle experience, use as a foaming face wash by scooping out a small amount and adding a little water. Apply to the face in gentle, circular motions before rinsing away. As it foams when washing off you’ll also notice it changes colour from deep purple to light pink -&nbsp; leaving you looking radiant.`;
 
   return (
     <>
@@ -34,7 +37,15 @@ export default function Product() {
               style={styles.detailsWrapper}
             >
               <LushFont style={styles.title}>{item.name}</LushFont>
-              <RenderedHtml sentence={item.description} />
+              <RenderHtml
+                contentWidth={100}
+                source={{ html }}
+                tagsStyles={{
+                  a: { textDecorationLine: "underline" },
+                  b: { fontWeight: "bold" },
+                  i: { fontStyle: "italic" },
+                }}
+              />
             </LinearBackground>
           </ScrollView>
         </SafeAreaView>
