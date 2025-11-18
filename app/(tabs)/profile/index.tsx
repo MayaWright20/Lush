aaf05923ad5bb59109fd2b98eeb7e8c8f9155288
mayagwright20 <mayagwright20@hotmail.com>
2025-11-18 01:01:17 +0000

    Remove unnecessary username

--- app/(tabs)/profile/index.tsx
+++ app/(tabs)/profile/index.tsx
@@ -3,20 +3,16 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import CheckedBackground from "@/components/backgrounds/checked-background";
 import CTA from "@/components/cta";
-import { LushFont } from "@/components/lush-font";
 import { COLORS } from "@/constants/colors";
-import { FONT_SIZE_HEADER, PADDING_HORIZONTAL_PAGE } from "@/constants/styles";
+import { PADDING_HORIZONTAL_PAGE } from "@/constants/styles";
 import useProfile from "@/hooks/useProfile";
 
 export default function Profile() {
-  const { logout, userName } = useProfile();
+  const { logout } = useProfile();
 
   return (
     <CheckedBackground isOnlyBorders borderColor={COLORS.BLUE}>
       <SafeAreaView style={styles.safeAreaView}>
-        <LushFont
-          style={{ fontSize: FONT_SIZE_HEADER }}
-        >{`Hi ${userName}`}</LushFont>
         <CTA title={"Logout"} onPress={logout} />
       </SafeAreaView>
     </CheckedBackground>
